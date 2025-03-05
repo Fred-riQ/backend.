5cf491c12b91f6590fbf5a03ec2d551413f3c229
Fred-riQ <fredrickbet5@gmail.com>
2025-03-05 23:11:10 +0300

    trial

--- trials1/backend/app/email/__init__.py
+++
@@ -1,8 +0,0 @@
-from .extensions import mail
-from .email import send_booking_confirmation  # Import the send_booking_confirmation function
-
-def init_email(app):
-    """
-    Initialize the email module with the Flask app.
-    """
-    mail.init_app(app)
--- trials1/backend/app/email/extensions.py
+++
@@ -1,4 +0,0 @@
-from flask_mail import Mail
-
-# Initialize Flask-Mail
-mail = Mail()
